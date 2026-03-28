be9ba0438345e092d29cf2d03ba797293b5e1ebe
yuuki <y.clover.2027@gmail.com>
2026-03-28 11:21:01 +0900

    最小化修正

--- 自動印刷プログラム.bat
+++ 自動印刷プログラム.bat
@@ -1,8 +1,5 @@
 @echo off
 pushd "%~dp0"
-powershell -ExecutionPolicy Bypass -File "自動印刷プログラム.ps1"
-if %errorlevel% neq 0 (
-    echo Error occurred.
-    pause
-)
+start /min powershell -WindowStyle Minimized -ExecutionPolicy Bypass -File "自動印刷プログラム.ps1"
 popd
+exit
